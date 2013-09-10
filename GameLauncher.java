aa6c655298dc3ddbe7f248aa675091ac613b29bf
InfinityPenguin <aidan129@sbcglobal.net>
2013-09-10 02:14:39 -0700

    Update GameLauncher.java
    Final version

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,21 +1,14 @@
+package generator;
+import java.io.IOException;
+
 class GameLauncher {
-  public static void main(String args[]) {
-    while (true) {
-      Game game = new Game();
-      game.play();
-      if (!Yes("Play again? (y/n) ")) break;
-    }
-    System.out.println("Thanks for playing!");
-  }
-  
-  private static boolean Yes(String prompt) {
-    while (true) {
-      System.out.print(prompt);
-      Scanner scanner = new Scanner(System.in);
-      String input = scanner.nextLine();
-      if (input.equals("y") || input.equals("Y")) return true;
-      if (input.equals("n") || input.equals("N")) return false;
-      System.out.println("Please enter y or n.");
-    }
-  }
+	public static void main(String[] args) throws IOException { 
+			Game game = new Game();
+			while (true) {
+				game.Initialize();
+				game.play();
+				if (!Utility.ask("Play again?")) break;
+			}
+			System.out.println("Thanks for playing!");
+	}
 }
